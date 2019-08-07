bfa67b0b110388ba8932b384cc1299024cf588e9
PCwik88 <paul@cwik.us>
2019-08-07 11:18:18 -0500

    books routes complete

---
+++ routes/api/books.js
@@ -0,0 +1,18 @@
+// Dependencies
+const router = require('express').Router()
+
+// File Imports
+const booksController = require('../controllers/booksController');
+
+// Matches with '/api/books'
+router.route('/')
+    .get(booksController.findAll);
+
+// matches with '/api/books/:id'
+router.route(':id')
+    .get(booksController.findById)
+    .post(booksController.addBook)
+    .put(booksController.updateBook)
+    .delete(booksController.deleteById);
+
+module.exports = router;
